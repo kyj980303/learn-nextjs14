8a0fffcab8b726c3884c79b48393e592512ae1c9
kyj980303 <rladuswjd0303@naver.com>
2024-05-15 16:40:36 +0900

    Feat: show error componet when an error occurs

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,9 +1,10 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
+  throw new Error("something broke...");
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
